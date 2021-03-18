9f87ce3f911e18c192eed9a3333ff19f6e5427f1
Andrew Casey <andrew.casey@microsoft.com>
2021-03-18 13:12:12 -0700

    Introduce a mechanism for not skipping trivia when normalizing positions

--- normalize-positions.ts
+++ normalize-positions.ts
@@ -13,8 +13,13 @@ interface PositionWrapper {
     lineChar?: LineChar;
 }
 
+let freezeCache: LineChar = [-1, -1];
 function freeze(lineChar: MutableLineChar): LineChar {
-    return [lineChar[0], lineChar[1]];
+    if (lineChar[0] !== freezeCache[0] || lineChar[1] !== freezeCache[1]) {
+        freezeCache = [lineChar[0], lineChar[1]];
+    }
+
+    return freezeCache;
 }
 
 function compareOffsets(a: number, b: number): number {
@@ -25,6 +30,12 @@ function compareLineChars(a: LineChar, b: LineChar): number {
     return a[0] - b[0] || a[1] - b[1];
 }
 
+/**
+ * @param stream A stream of string chunks with respect to which `positions` should be normalized.
+ * @param positions Positions to be normalized.  NB: negative numbers will be treated as positions
+ * that should be converted to line-char without but not moved past trivia.
+ * @returns A list of normalized line-char positions.
+ */
 function normalizePositions(stream: stream.Readable, positions: ReadonlyArray<number | LineChar>): Promise<ReadonlyArray<LineChar>> {
     return positions.length === 0 ? Promise.resolve([]) : new Promise<ReadonlyArray<LineChar>>((resolve, reject) => {
         let prevCh = -1;
@@ -100,31 +111,53 @@ function normalizePositions(stream: stream.Readable, positions: ReadonlyArray<nu
             CharClassEscapeCloseBracket,
         }
 
-        // We partition the positions because the two varieties
+        // We partition the positions because the varieties
         // cannot be sorted with respect to each other.
-        const offsetWrappers: PositionWrapper[] = [];
-        const lineCharWrappers: PositionWrapper[] = [];
+        const fixedOffsetWrappers: PositionWrapper[] = []; // Don't skip trivia
+        const offsetWrappers: PositionWrapper[] = []; // Do skip trivia
+        const fixedLineCharWrappers: PositionWrapper[] = []; // Don't skip trivia
+        const lineCharWrappers: PositionWrapper[] = []; // Do skip trivia
 
         for (let i = 0; i < positions.length; i++) {
             const position = positions[i];
             if (typeof position === "number") {
-                offsetWrappers.push({
-                    returnIndex: i,
-                    offset: position as number,
-                });
+                if (position < 0) {
+                    fixedOffsetWrappers.push({
+                        returnIndex: i,
+                        offset: -position,
+                    });
+                }
+                else {
+                    offsetWrappers.push({
+                        returnIndex: i,
+                        offset: position,
+                    });
+                }
             }
             else {
-                lineCharWrappers.push({
-                    returnIndex: i,
-                    lineChar: position as LineChar,
-                });
+                if (position[0] < 0 || position[1] < 0) {
+                    fixedLineCharWrappers.push({
+                        returnIndex: i,
+                        lineChar: [Math.abs(position[0]), Math.abs(position[1])],
+                    });
+                }
+                else {
+                    lineCharWrappers.push({
+                        returnIndex: i,
+                        lineChar: position,
+                    });
+                }
             }
         }
 
+        fixedOffsetWrappers.sort((a, b) => compareOffsets(a.offset!, b.offset!));
         offsetWrappers.sort((a, b) => compareOffsets(a.offset!, b.offset!));
+        fixedLineCharWrappers.sort((a, b) => compareLineChars(a.lineChar!, b.lineChar!));
         lineCharWrappers.sort((a, b) => compareLineChars(a.lineChar!, b.lineChar!));
 
+        let fixedOffsetWrappersPos = 0;
         let offsetWrappersPos = 0;
+        let fixedLineCharWrappersPos = 0;
         let lineCharWrappersPos = 0;
 
         let currOffset = 0;
@@ -372,17 +405,25 @@ function normalizePositions(stream: stream.Readable, positions: ReadonlyArray<nu
             // This is handy when debugging
             // console.error(`${currOffset}\t${/^[a-zA-Z0-9!@#$%^&*()[\]{}\\/;':"<,>.?`~+=_\-]$/.test(String.fromCharCode(ch)) ? String.fromCharCode(ch) : "0x" + ch.toString(16)}\t(${currLineChar[0]},${currLineChar[1]})\t${isTrivia ? "Triv" : "Not"}\tS${state}\tB${braceDepth}`);
 
-            if (!isTrivia) {
-                const frozenLineChar = freeze(currLineChar);
+            for (; fixedOffsetWrappersPos < fixedOffsetWrappers.length && compareOffsets(fixedOffsetWrappers[fixedOffsetWrappersPos].offset!, currOffset) <= 0; fixedOffsetWrappersPos++) {
+                fixedOffsetWrappers[fixedOffsetWrappersPos].offset = currOffset;
+                fixedOffsetWrappers[fixedOffsetWrappersPos].lineChar = freeze(currLineChar);
+            }
 
+            for (; fixedLineCharWrappersPos < fixedLineCharWrappers.length && compareLineChars(fixedLineCharWrappers[fixedLineCharWrappersPos].lineChar!, currLineChar) <= 0; fixedLineCharWrappersPos++) {
+                fixedLineCharWrappers[fixedLineCharWrappersPos].offset = currOffset;
+                fixedLineCharWrappers[fixedLineCharWrappersPos].lineChar = freeze(currLineChar);
+            }
+
+            if (!isTrivia) {
                 for (; offsetWrappersPos < offsetWrappers.length && compareOffsets(offsetWrappers[offsetWrappersPos].offset!, currOffset) <= 0; offsetWrappersPos++) {
                     offsetWrappers[offsetWrappersPos].offset = currOffset;
-                    offsetWrappers[offsetWrappersPos].lineChar = frozenLineChar;
+                    offsetWrappers[offsetWrappersPos].lineChar = freeze(currLineChar);
                 }
 
                 for (; lineCharWrappersPos < lineCharWrappers.length && compareLineChars(lineCharWrappers[lineCharWrappersPos].lineChar!, currLineChar) <= 0; lineCharWrappersPos++) {
                     lineCharWrappers[lineCharWrappersPos].offset = currOffset;
-                    lineCharWrappers[lineCharWrappersPos].lineChar = frozenLineChar;
+                    lineCharWrappers[lineCharWrappersPos].lineChar = freeze(currLineChar);
                 }
             }
 
@@ -408,6 +449,16 @@ function normalizePositions(stream: stream.Readable, positions: ReadonlyArray<nu
 
             const eofLineChar = freeze(currLineChar);
 
+            for (let i = 0; i < fixedOffsetWrappersPos; i++) {
+                const wrapper = fixedOffsetWrappers[i];
+                result[wrapper.returnIndex] = wrapper.lineChar!;
+            }
+
+            for (let i = fixedOffsetWrappersPos; i < fixedOffsetWrappers.length; i++) {
+                const wrapper = fixedOffsetWrappers[i];
+                result[wrapper.returnIndex] = eofLineChar;
+            }
+
             for (let i = 0; i < offsetWrappersPos; i++) {
                 const wrapper = offsetWrappers[i];
                 result[wrapper.returnIndex] = wrapper.lineChar!;
@@ -415,6 +466,16 @@ function normalizePositions(stream: stream.Readable, positions: ReadonlyArray<nu
 
             for (let i = offsetWrappersPos; i < offsetWrappers.length; i++) {
                 const wrapper = offsetWrappers[i];
+                result[wrapper.returnIndex] = eofLineChar;
+            }
+
+            for (let i = 0; i < fixedLineCharWrappersPos; i++) {
+                const wrapper = fixedLineCharWrappers[i];
+                result[wrapper.returnIndex] = wrapper.lineChar!;
+            }
+
+            for (let i = fixedLineCharWrappersPos; i < fixedLineCharWrappers.length; i++) {
+                const wrapper = fixedLineCharWrappers[i];
                 result[wrapper.returnIndex] = eofLineChar;
             }
 
